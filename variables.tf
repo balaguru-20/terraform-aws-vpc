3ccabd77dfc3578fe7ef206320bc63942bdaca93
balaguru-20 <balaguravaiah0606@gmail.com>
2025-04-30 00:16:48 +0530

    terraform module for vpc

--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
 variable "project_name" {
-  
+
 }
 
 variable "environment" {
-  
+
 }
 
 variable "vpc_cidr" {
-      
+
 }
 
 variable "enable_dns_hostnames" {
-    default = true   
+  default = true
 }
 
 variable "common_tags" {
-    type = map
-    #default = {} #optional
+  type = map(any)
+  #default = {} #optional
 }
 
 variable "vpc_tags" {
-    default = {} 
+  default = {}
 }
